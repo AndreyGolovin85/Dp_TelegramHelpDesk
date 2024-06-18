d2012a6b42df02997977adf436b3b9c892bb4219
Sorakaze Aki <soraaki000@gmail.com>
2024-06-18 14:04:54 +0500

    - Добавил в .env.sample переменную для ид админа - Команда `/check_admin` для проверки наличия прав админа

--- bot/bot.py
+++ bot/bot.py
@@ -13,6 +13,7 @@ logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
 bot = Bot(token=os.getenv("API_TOKEN"))
+admin_id = int(os.getenv("ADMIN_ID"))
 dispatcher = Dispatcher()
 
 tickets = [{"user_id": 0, "title": "Тестовое название", "description": "Тестовое описание", "status": "new"}]
@@ -41,17 +42,29 @@ async def cmd_add_ticket(message: types.Message, command: CommandObject):
     if command.args is None:
         await message.reply("Proper usage of this command: */new_ticket <your issue here>*",
                             parse_mode=ParseMode.MARKDOWN)
-    else:
-        tickets.append(
-            {"user_id": message.chat.id, "title": f"{message.from_user.full_name}'s issue", "description": command.args,
-             "status": "new"})
-        reply = as_list(
-            f"User ID: {tickets[-1]['user_id']}",
-            f"Title: {tickets[-1]['title']}",
-            f"Description: {tickets[-1]['description']}",
-            f"Status: {tickets[-1]['status']}",
-            sep='\n')
-        await message.reply(**reply.as_kwargs())
+        return
+    tickets.append(
+        {
+            "user_id": message.chat.id,
+            "title": f"{message.from_user.full_name}'s issue",
+            "description": command.args,
+            "status": "new"
+        })
+    reply = as_list(
+        f"User ID: {tickets[-1]['user_id']}",
+        f"Title: {tickets[-1]['title']}",
+        f"Description: {tickets[-1]['description']}",
+        f"Status: {tickets[-1]['status']}",
+        sep='\n')
+    await message.reply(**reply.as_kwargs())
+
+
+@dispatcher.message(Command("check_admin"))
+async def cmd_check_authority(message: types.Message):
+    if message.chat.id == admin_id:
+        await message.reply("Admin authority confirmed.")
+        return
+    await message.reply("Missing admin privileges.")
 
 
 async def main():
