fe798bcbc0c07775cc2d8d11ac41044087b2088f
AndreyGolovin85 <47321231+AndreyGolovin85@users.noreply.github.com>
2024-06-24 23:41:03 +0300

    Рефакторинг

--- bot/bot.py
+++ bot/bot.py
@@ -6,7 +6,8 @@ from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command, CommandObject
-from aiogram.utils.formatting import as_list
+
+from utils import tickets_write, reply_list, cmd_tickets_new, cmd_tickets_none, cmd_tickets_not_admin
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,19 +17,6 @@ bot = Bot(token=os.getenv("API_TOKEN"))
 admin_id = int(os.getenv("ADMIN_ID"))
 dispatcher = Dispatcher()
 
-tickets = [{"user_id": 0, "title": "Тестовое название", "description": "Тестовое описание", "status": "test"}]
-
-
-def reply_list(item=None):
-    if item is None:
-        item = tickets[-1]
-    return as_list(
-        f"User ID: {item['user_id']}",
-        f"Title: {item['title']}",
-        f"Description: {item['description']}",
-        f"Status: {item['status']}",
-        sep='\n')
-
 
 @dispatcher.message(Command("start"))
 async def cmd_start(message: types.Message):
@@ -37,26 +25,23 @@ async def cmd_start(message: types.Message):
 
 @dispatcher.message(Command("tickets"))
 async def cmd_tickets(message: types.Message, command: CommandObject):
-    if message.chat.id != admin_id:
-        if command.args is not None:
+    command_args = command.args
+    user_id = message.chat.id
+    if user_id != admin_id:
+        if command_args is not None:
             await message.answer("! Do not insert arguments here !")
-        for item in tickets:
-            if message.chat.id == item['user_id']:
-                reply_text = reply_list(item)
-                await message.answer(**reply_text.as_kwargs())
+        reply_text = cmd_tickets_not_admin(user_id)
+        await message.answer(**reply_text.as_kwargs())
         return
 
-    if command.args == "new":
-        for item in tickets:
-            if item['status'] == "new":
-                reply_text = reply_list(item)
-                await message.answer(**reply_text.as_kwargs())
+    if command_args == "new":
+        reply_text = cmd_tickets_new(command_args)
+        await message.answer(**reply_text.as_kwargs())
         return
 
-    if command.args is None:
-        for item in tickets:
-            reply_text = reply_list(item)
-            await message.answer(**reply_text.as_kwargs())
+    if command_args is None:
+        reply_text = cmd_tickets_none()
+        await message.answer(**reply_text.as_kwargs())
         return
 
 
@@ -67,12 +52,11 @@ async def cmd_add_ticket(message: types.Message, command: CommandObject):
                             parse_mode=ParseMode.MARKDOWN)
         return
 
-    ticket = {
-        "user_id": message.chat.id,
-        "title": f"{message.from_user.full_name}'s issue",
-        "description": command.args,
-        "status": "new"}
-    tickets.append(ticket)
+    user_id = message.chat.id
+    title = f"{message.from_user.full_name}'s issue"
+    description = command.args
+
+    ticket = tickets_write(description, title, user_id)
     reply_text = reply_list(ticket)
     await message.reply(**reply_text.as_kwargs())
     await bot.send_message(chat_id=admin_id, text=f"Новая заявка: \n{reply_text.as_html()}")
