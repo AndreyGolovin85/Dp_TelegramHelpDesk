6ae9f71d2e83aaaf79a838e94e62121bc5bca904
Sorakaze Aki <soraaki000@gmail.com>
2024-07-02 21:24:01 +0500

    перекинул сессии внутрь методов

--- bot/db.py
+++ bot/db.py
@@ -7,7 +7,7 @@ class Base(DeclarativeBase):
     pass
 
 
-class Ticket(Base):
+class Ticket(Base, sessionmaker):
     __tablename__ = "tickets"
     id: Mapped[int] = mapped_column(primary_key=True)
     uid: Mapped[int] = mapped_column(Integer)
@@ -21,50 +21,51 @@ class Ticket(Base):
     # Возвращает список словарей тикетов
     @classmethod
     def list_tickets(cls, uid=0, status: str | None = None) -> list[dict]:
-        if uid != 0:
-            select_tickets = select(Ticket).where(uid == Ticket.uid)
-        elif status is None:
-            select_tickets = select(Ticket)
-        else:
-            select_tickets = select(Ticket).where(status == Ticket.status)
+        with Session() as session:
+            if uid != 0:
+                select_tickets = select(Ticket).where(uid == Ticket.uid)
+            elif status is None:
+                select_tickets = select(Ticket)
+            else:
+                select_tickets = select(Ticket).where(status == Ticket.status)
 
-        tickets_dict = []
-        for ticket in session.query(select_tickets.subquery()).all():
-            tickets_dict.append({
-                "user_id": ticket.uid,
-                "title": ticket.title,
-                "description": ticket.description,
-                "status": ticket.status})
-        return tickets_dict
+            tickets_dict = []
+            for ticket in session.query(select_tickets.subquery()).all():
+                tickets_dict.append({
+                    "user_id": ticket.uid,
+                    "title": ticket.title,
+                    "description": ticket.description,
+                    "status": ticket.status})
+            return tickets_dict
 
     # Редактирует статус тикетов в БД
     @classmethod
     async def edit_ticket_status(cls, ticket_dict: dict, new_status: str):
-        list_ticket = select(Ticket).where(
-            and_(
-                Ticket.uid == ticket_dict["user_id"],
-                Ticket.title == ticket_dict["title"],
-                Ticket.description == ticket_dict["description"],
-                Ticket.status == ticket_dict["status"]))
-        ticket = session.scalars(list_ticket).first()
-        ticket.status = new_status
-        session.commit()
+        with Session() as session:
+            list_ticket = select(Ticket).where(
+                and_(
+                    Ticket.uid == ticket_dict["user_id"],
+                    Ticket.title == ticket_dict["title"],
+                    Ticket.description == ticket_dict["description"],
+                    Ticket.status == ticket_dict["status"]))
+            ticket = session.scalars(list_ticket).first()
+            ticket.status = new_status
+            session.commit()
 
     # Запись тикетов в БД
     @classmethod
     async def add_ticket(cls, ticket_dict: dict):
-        new_ticket = Ticket(
-            uid=ticket_dict["user_id"],
-            title=ticket_dict["title"],
-            description=ticket_dict["description"],
-            status=ticket_dict["status"]
-           )
-        session.add(new_ticket)
-        session.commit()
+        with Session() as session:
+            new_ticket = Ticket(
+                uid=ticket_dict["user_id"],
+                title=ticket_dict["title"],
+                description=ticket_dict["description"],
+                status=ticket_dict["status"]
+            )
+            session.add(new_ticket)
+            session.commit()
 
 
 engine = create_engine("sqlite:///bot.db", echo=True)
 Base.metadata.create_all(engine)
 Session = sessionmaker(autoflush=False, bind=engine)
-
-session = Session()
